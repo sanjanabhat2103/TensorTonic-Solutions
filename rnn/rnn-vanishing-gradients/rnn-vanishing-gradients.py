97925bbe89fd4f49d12ace7f7d6ca1bb282d93d9
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-06 19:04:07 +0530

    Solve rnn-vanishing-gradients

--- rnn/rnn-vanishing-gradients/rnn-vanishing-gradients.py
+++ rnn/rnn-vanishing-gradients/rnn-vanishing-gradients.py
@@ -5,14 +5,9 @@ def compute_gradient_norm_decay(T: int, W_hh: np.ndarray) -> list:
     Simulate gradient norm decay over T time steps.
     Returns list of gradient norms.
     """
-    W_hh = np.asarray(W_hh, dtype=float)
-
-    # Spectral norm (largest singular value)
-    spectral_norm = np.linalg.norm(W_hh, ord=2)
-
+    W_hh = np.asarray(W_hh, dtype = float)
+    spectral_norm = np.linalg.norm(W_hh, ord = 2)
     norms = []
-
     for t in range(T):
         norms.append(float(spectral_norm ** t))
-
     return norms
